549748ab9276901c764ef020f9e6a86f795f6f4a
caioRafael <caiorafaelrg@gmail.com>
2022-08-12 20:22:51 -0300

    animations

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,33 +1,41 @@
-import { FC, useRef, useState } from "react";
+import { FC, useCallback, useRef, useState } from "react";
 import { SafeAreaView, Text, TouchableOpacity, View } from "react-native";
-import Lottie from 'lottie-react-native'
+import Lottie from 'lottie-react-native';
 
 import {styles} from './styles'
 
 import breathe from '../../animations/breathe.json'
 
 const Home: FC = () => {
-    const {container} = styles
+    const {container, button, textButton} = styles
     const [loop, setLoop] = useState<boolean>(false)
-    const animation = useRef(null)
+    const animation = useRef<Lottie>(null)
 
-    const startAnimation = () => {
+    const startAnimation = useCallback(() => {
+        setLoop(!loop)
         animation.current?.reset()
         animation.current?.play()
-        setLoop(!loop)
-    }
-    return ( 
-        <SafeAreaView style={container}>
-            <Lottie autoSize source={breathe} autoPlay loop={loop} ref={animation} progress={30000}/>
+        
+        if(!loop === false){
+            animation.current?.pause()
+        }
+    }, [loop])
 
-            <TouchableOpacity
-                onPress={startAnimation}
-            >
-                <Text>
-                    Iniciar
-                </Text>
-            </TouchableOpacity>
-        </SafeAreaView>
+    return ( 
+        <View>
+            <SafeAreaView style={container}>
+                <Lottie autoSize source={breathe} autoPlay loop={loop} ref={animation} duration={20000}/>
+
+                <TouchableOpacity
+                    onPress={startAnimation}
+                    style={button}
+                >
+                    <Text style={textButton}>
+                        {loop ? 'Parar' : 'Iniciar'}
+                    </Text>
+                </TouchableOpacity>
+            </SafeAreaView>
+        </View>
      );
 }
  
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -8,4 +8,19 @@ export const styles = StyleSheet.create({
         justifyContent: 'center',
         width: '100%'
     },
+
+    button:{
+        width: 200,
+        height: 50,
+        borderRadius: 10,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#87BEF9'
+    },
+
+    textButton:{
+        fontSize: 20,
+        fontWeight: '700',
+        color: '#fff'
+    }
 })
